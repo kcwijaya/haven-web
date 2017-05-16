aca52915b0429ab2cda7718c7f483669dbe3f01d
Kimberly Wijaya <kcwijaya@stanford.edu>
2017-05-16 03:41:31 -0700

    fixed bugs in editing and viewint tasks  Please enter the commit message for your changes. Lines starting

--- scripts/taskview.js
+++ scripts/taskview.js
@@ -1,4 +1,73 @@
+function getTaskSkills(id)
+{
+  var params = $.param({
+    id: parseInt(id),
+  });
+
+	$.ajax({
+		type: "GET",
+		url: "/tasks/skills?" + params,
+		success: function (res){
+			console.log("SKILLS: " );
+			console.log(res);
+			var categories = {};
+			for (i = 0; i < res.length; i++)
+			{
+				if (!(res[i].category in categories))
+				{
+					categories[res[i].category] = [];
+				}
+				categories[res[i].category].push(res[i].name);
+			}
+
+			var toAppend = "";
+
+			Object.keys(categories).forEach(function(key,index) {
+				toAppend += "<b><h7>" + key + "</h7></b>"
+				toAppend += "<ul>"
+				for (i = 0; i < categories[key].length; i++)
+				{
+					toAppend += "<li>" + categories[key][i] + "</li>";
+				}
+				toAppend += "</ul>";
+			});
+
+			$('#skills-text').html(toAppend);
+		}
+	});
+}
+
+function getTaskDisclaimers(id)
+{
+  var params = $.param({
+    id: parseInt(id),
+  });
+
+	$.ajax({
+		type: "GET",
+		url: "/tasks/disclaimers?" + params,
+		success: function (res){
+			console.log("DISCLAIMERS: " );
+			console.log(res);
+			var toAppend = "<ul>";
+			for (i = 0; i < res.length; i++)
+			{
+				toAppend += "<li>" + res[i].disclaimer + "</li>";
+			}
+			toAppend += "</ul>";
+
+			$('#disclaimers-text').html(toAppend);
+		}
+	});
+}
+
+
 $(document).ready(function (){
+
+	var id = $("#id").text().trim();
+
+	getTaskDisclaimers(id);
+	getTaskSkills(id);
 	$('#edit-task').click(function(e){
 		var id = $('#id').text().trim();
 		var volunteers = [];
